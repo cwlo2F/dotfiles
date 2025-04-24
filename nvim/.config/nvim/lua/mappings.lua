1b00700e5b7fe351e9a8dc5bd82ac6ec1be64ac8
cwlo2F <cwlo2F@gmail.com>
2025-04-24 23:16:38 +0900

    Migrate from coc to nvim-cmp

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -2,24 +2,14 @@
 vim.keymap.set('n', '<Leader>ev', ':vsplit $MYVIMRC<CR>')
 vim.keymap.set('n', '<Leader>sv', ':source $MYVIMRC<CR>')
 
-vim.keymap.set('i', '<C-J>',
-  'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<C-J>" : coc#refresh()',
-  { noremap = true, silent = true, expr = true })
-vim.keymap.set('i', '<C-K>',
-  'coc#pum#visible() ? coc#pum#prev(1) : "<C-K>"',
-  { noremap = true, silent = true, expr = true })
-vim.keymap.set('i', '<CR>',
-  [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<C-R>=coc#on_enter()\<CR>"]],
-  { noremap = true, silent = true, expr = true })
-
 local ls = require("luasnip")
 
-vim.keymap.set('i', '<Tab>', function() 
+vim.keymap.set('i', '<Tab>', function()
   if ls.expandable() then return "<Plug>luasnip-expand-snippet" else return "<Tab>" end end,
   { noremap = false, expr = true, silent = true })
 
 -- Jump forward in through tabstops in insert and visual mode with Control-f
-vim.keymap.set({'i', 's'}, '<C-F>', 
+vim.keymap.set({'i', 's'}, '<C-F>',
   function() ls.jump(1)  end,
   { silent = true })
 
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -42,11 +42,62 @@ return {
     config = true,
   },
   {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "mason.nvim", "nvim-lspconfig" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "clangd", "lua_ls", "pyright" },
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     dependencies = { "williamboman/mason.nvim" },
     config = function()
-      require("lspconfig").lua_ls.setup {}
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup {
+        settings = { Lua = {diagnostics = { globals = { "vim" } } } },
+      }
+
+      lspconfig.clangd.setup {}
+
+      lspconfig.pyright.setup {}
     end
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    version = false,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"]      = cmp.mapping.confirm({ select = true }),
+          ["<C-n>"]     = cmp.mapping.select_next_item(),
+          ["<C-p>"]     = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
   },
 
   "rust-lang/rust.vim",
